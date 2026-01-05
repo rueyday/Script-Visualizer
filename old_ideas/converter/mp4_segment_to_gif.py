cff168655dd6a3f89e6a198d415128d15875c29e
Ruey-Day <rueyday1@gmail.com>
2026-01-05 17:31:26 -0500

    fixed some weird dependency error

--- old_ideas/converter/mp4_segment_to_gif.py
+++ old_ideas/converter/mp4_segment_to_gif.py
@@ -1,21 +1,21 @@
-from moviepy import VideoFileClip
+from moviepy.editor import VideoFileClip
 
-INPUT_VIDEO = "../../final_hallway.mp4"     # path to your MP4
-OUTPUT_GIF = "../../hallway.gif"
+INPUT_VIDEO = "../recursive-bayesian-estimators.mp4"
+OUTPUT_GIF = "../recursive-bayesian-estimators.gif"
 
 START_TIME = 0.0
-END_TIME = 16.5
+END_TIME = 30
 
-FPS = 15
-SCALE = 3.0
+FPS = 12
+SCALE = 0.9
 
 clip = VideoFileClip(INPUT_VIDEO)
 
-# MoviePy 2.x uses subclipped()
-subclip = clip.subclipped(START_TIME, END_TIME)
+# MoviePy 1.x uses subclip()
+subclip = clip.subclip(START_TIME, END_TIME)
 
 # Resize
-subclip = subclip.resized(SCALE)
+subclip = subclip.resize(SCALE)
 
 # Write GIF
 subclip.write_gif(OUTPUT_GIF, fps=FPS)
